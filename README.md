5cf19cc2168eac78953a710eecf79ddcae973600
Masao-Taketani <taket003@umn.edu>
2020-07-12 00:28:45 +0900

    start implementing a func to preprocess utkface

--- README.md
+++ README.md
@@ -12,10 +12,11 @@ tmp image<br>
   [UTKFace](https://susanqq.github.io/UTKFace/)
 
 ## References
-- Paper
+- Papers
   - [Xception: Deep Learning with Depthwise Separable Convolutions](https://arxiv.org/abs/1610.02357)<br>
   - [Deep Residual Learning for Image Recognition](https://arxiv.org/abs/1512.03385)<br>
   - [Identity Mappings in Deep Residual Networks](https://arxiv.org/abs/1603.05027)
 - Repos
   - https://github.com/yanchummar/xception-keras<br>
   - https://github.com/raghakot/keras-resnet<br>
+  - https://github.com/qqwweee/keras-yolo3<br>
--- utils/__init__.py
+++ utils/__init__.py
@@ -60,6 +60,57 @@ def train_test_split(X, y, random_state=0, test_size=0.1):
     return X_train, X_test, y_train, y_test
 
 
+def preprocess_wider_face(gt_file, img_dir):
+    print("start preprocessing {} data.".format(gt_file.split("_")[-3]))
+
+    with open(gt_file, "r") as f_r:
+        num_of_train_imgs = f_r.read().count('jpg')
+        img_paths = []
+
+        # loop for each img
+        for _ in tqdm(range(num_of_train_imgs)):
+            # use strip() to get rid of "\n"
+            img_path = f_r.readline().strip()
+            img_paths.append(img_path)
+            full_img_path = os.path.join(img_dir, img_path)
+            im = cv2.imread(full_img_path)
+            im_h, im_w, _ = im.shape
+
+            splited_img_path = img_path.split("/")
+            dir_name = splited_img_path.[0]
+            txt_fname = splited_img_path[1].replace(".jpg", ".txt")
+
+            num_faces = int(f.readline())
+            gt_list = []
+
+            # loop for each bbox label
+            for _ in range(num_faces):
+                xmin, ymin, w, h, _, _, _, _, _, _ = f_r.readline().split()
+                # get bbox coordinate values in ratios of the overall image size
+                cx = str((int(xmin) + int(w) / 2) / im_w)
+                cy = str((int(ymin) + int(h) / 2) / im_h)
+                rw = str(w / im_w)
+                rh = str(h / im_h)
+
+                class_id = "0"
+
+                label = " ".join([class_id, cx, cy, rw, rh])
+                gt_list.append(label)
+
+            gt_str = "\n".join(gt_list)
+
+            # create a label file for each img
+            with open(os.path.join(img_dir, dir_name, txt_fname), "w") as f_w:
+                f_w.write(gt_str)
+
+    print("preprocessing {} data is done.".format(gt_file.split("_")[-3]))
+
+
+def preprocess_utkface():
+    # annotation format: [age]_[gender]_[race]_[date&time].jpg
+    pass
+
+
 def normalize_inputs(X_train, X_test):
     X_train = X_train.astype("float32") / 255.0
     X_test = X_test.astype("float32") / 255.0
